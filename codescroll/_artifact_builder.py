48bf810bbac72a52e49ae6dbd8aee35e019c58fb
HansolChoe <hansol614@gmail.com>
2022-04-06 16:00:58 +0900

    Fix _ArtifactBuilder to use the function in cslib

--- codescroll/_artifact_builder.py
+++ codescroll/_artifact_builder.py
@@ -57,8 +57,7 @@ class _ArtifactBuilder(Runner):
                     src = dependency
                     if not os.path.isfile(src):
                         continue
-                    file_extension_list = [".c", ".cpp", ".h", ".hpp"]
-                    if not any([dependency.endswith(i) for i in file_extension_list]):
+                    if cslib.is_source_or_header_file(src):
                         continue
                     dest = artifacts_dir_path + dependency
                     if os.path.exists(dest):
